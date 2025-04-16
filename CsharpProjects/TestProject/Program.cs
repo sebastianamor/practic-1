71cfb830f075cb9b63fcdd1dce9f8204d55221dd
sebastian lapt <smoreira11@hotmail.com>
2025-04-17 08:12:23 +0900

    .netxS

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+int result = Random.Next();
--- program.cs
+++
@@ -1,3 +0,0 @@
-// See https://aka.ms/new-console-template for more information
-console.WriteLine("hello")
-Console.WriteLine("Hello, World!");
