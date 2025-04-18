0231b24c9ed097015acd9d8b7e54580bfbd20652
sebastian lapt <smoreira11@hotmail.com>
2025-04-18 13:26:27 +0900

    desafio

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,10 +1,5 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
-
-dice
+Console.WriteLine(largerValue);
